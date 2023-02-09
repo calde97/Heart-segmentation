2105ea2e0a378b75de5f846f7b3ea92edeaad7d6
calde <juanfelipe.calderon@mail.polimi.it>
2023-02-09 10:24:07 +0100

    fix first training script

--- training/train.py
+++ training/train.py
@@ -35,7 +35,7 @@ def dice_loss(predicted, labels):
 
 
 if __name__ == '__main__':
-    batch_size = 8
+    batch_size = 2
     num_epochs = 10
     lr = 0.0001
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -43,18 +43,18 @@ if __name__ == '__main__':
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Resize((256, 256)),
-        transforms.RandomVerticalFlip(p=0.5),
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomRotation(degrees=180)
+        #transforms.RandomVerticalFlip(p=0.5),
+        #transforms.RandomHorizontalFlip(p=0.5),
+        #transforms.RandomRotation(degrees=180)
     ])
     training_path = '../data/csv_files/train.csv'
     validation_path = '../data/csv_files/val.csv'
 
-    training_dataset = ImageSegmentationDataset(csv_file=training_path, transform=transform, limit_for_testing=5,
+    training_dataset = ImageSegmentationDataset(csv_file=training_path, transform=transform, limit_for_testing=10,
                                                 apply_hu_transformation=True, apply_windowing=True)
-    validation_dataset = ImageSegmentationDataset(csv_file=training_path, transform=transform, limit_for_testing=5)
+    validation_dataset = ImageSegmentationDataset(csv_file=training_path, transform=transform, limit_for_testing=10)
     train_loader = DataLoader(training_dataset, batch_size=batch_size, shuffle=False)
-    valid_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=True)
+    valid_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
 
     model = UNet(num_classes=1).to(device)
     # criterion = nn.CrossEntropyLoss()
@@ -64,11 +64,13 @@ if __name__ == '__main__':
     print("Number of learnable parameters: ", total_params)
     # add tqdm to show progress
 
-    best_val_loss = np.inf
+    best_val_loss = 1000
     best_val_iou = 0
 
-    for epoch in tqdm(range(num_epochs)):
-        for i, data in enumerate(train_loader):
+    for epoch in (outer_bar := tqdm(range(num_epochs))):
+        total_training_loss = 0
+        total_training_iou = 0
+        for i, data in (inner_bar := tqdm(enumerate(train_loader))):
             images, labels = data
             images = images.to(device)
             labels = labels.to(device)
@@ -76,15 +78,23 @@ if __name__ == '__main__':
             outputs = model(images)
             # apply sigmoid to the output
             outputs = torch.sigmoid(outputs)
+            loss = criterion(outputs, labels)
             # get the prediction by comparing the output with 0.5
             predicted = (outputs > 0.5).float()
-            loss = criterion(outputs, labels)
             iou = IoU(predicted, labels)
             loss.backward()
+            total_training_loss += loss.item()
+            total_training_iou += iou
             optimizer.step()
 
-            print(f'training loss: {loss}')
-            print(f'training IoU: {iou}')
+            inner_bar.set_description(f"Loss: {loss.item():.2f}, IoU: {iou:.2f}")
+
+        mean_loss = total_training_loss / len(train_loader)
+        outer_bar.set_description(f"Epoch: {epoch + 1}/{num_epochs}, Training loss: {mean_loss:.2f}")
+        outer_bar.set_description(f"Epoch: {epoch + 1}/{num_epochs}, Training loss: {mean_loss:.2f}")
+        outer_bar.set_description(f"Epoch: {epoch + 1}/{num_epochs}, Training IoU: {total_training_iou / len(train_loader):.2f}")
+
+
 
         # Evaluate the model on the validation set
         with torch.no_grad():
@@ -92,29 +102,25 @@ if __name__ == '__main__':
             total = 0
             index = 0
             iou = 0
+            val_loss_total = 0
             for i, data in enumerate(valid_loader):
                 images, labels = data
                 images = images.to(device)
                 labels = labels.to(device)
                 outputs = model(images)
                 outputs = torch.sigmoid(outputs)
-                predicted = (outputs > 0.5).float()
                 loss = criterion(outputs, labels)
-                total += (labels.size(-1) * labels.size(-2))
-                correct += (predicted == labels).sum().item()
+                val_loss_total += loss.item()
+                predicted = (outputs > 0.5).float()
                 iou += IoU(predicted, labels)
                 index += 1
-            accuracy = 100 * correct / total
-            mean_iou = iou / index
-            print("Epoch {}/{}, Accuracy: {:.2f}%".format(epoch + 1, num_epochs, accuracy))
-            print("Epoch {}/{}, Mean IoU: {:.2f}%".format(epoch + 1, num_epochs, mean_iou))
-            print("Epoch {}/{}, Validation loss: {:.2f}%".format(epoch + 1, num_epochs, loss))
+            mean_iou = iou / len(valid_loader)
+            mean_val_loss = val_loss_total / len(valid_loader)
+            print(f"Validation loss: {mean_val_loss:.2f}, Validation IoU: {mean_iou:.2f}")
+            print(f'Mean IoU: {mean_iou:.2f}')
 
-            # get current date in python with format dd/mm/YY
+        if mean_val_loss < best_val_loss and mean_val_loss < 0.80:
+            best_val_loss = mean_val_loss
+            torch.save(model.state_dict(), f"model{datetime.now().strftime('%Y_%m_%d__%H_%M_%S')}.pth")
+            print("Model saved")
 
-            if loss < best_val_loss:
-                now = datetime.now()
-                dt_string = now.strftime("%d/%m/%Y_%H:%M:%S")
-                best_val_loss = loss
-                torch.save(model.state_dict(), f"{dt_string}-{epoch}model.pt")
-                print("Model saved")
