b04af8c158bdd006e726a206d93d1afb8cb687f8
calde <juanfelipe.calderon@mail.polimi.it>
2023-02-19 23:01:48 +0100

    add model selection in the evaluation dashboard

--- dash-visualizations/evaluation_visualization.py
+++ dash-visualizations/evaluation_visualization.py
@@ -18,13 +18,12 @@ import plotly.express as px
 import os
 from natsort import natsorted
 
+
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+cwd = os.getcwd()
 
-df = pd.read_csv('../data/csv_files/val_evaluation.csv')
-
-# get all unique patients as a list
-patients = df['patient'].unique().tolist()
-
+# loop inside assets folder and get all the name of the folders
+# these folders are the patients
 # the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
     "position": "fixed",
@@ -48,12 +47,16 @@ sidebar = html.Div(
     [
         html.H2("Evaluations", className="display-4"),
         html.Hr(),
+        # select model
+        html.H6("Select model"),
+        dcc.Dropdown(id='dropdown_model', options=os.listdir(os.path.join(cwd, 'assets', 'models_evaluation')), value=''),
         html.H6("Select patient"),
-        dcc.Dropdown(id='dropdown', options=patients, value=''),
+        dcc.Dropdown(id='dropdown', options=[], value=''),
     ],
     style=SIDEBAR_STYLE,
 )
-{'src': f'assets/test/imagesAM01.png'}
+
+
 carousel = dbc.Carousel(
     id='carousel',
     items=[],
@@ -70,17 +73,41 @@ content = html.Div([dcc.Graph(id='bar-chart-1'),
 
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
+@app.callback(
+    Output("dropdown", "options"),
+    Input("dropdown_model", "value"),
+)
+def update_dropdown(model_name):
+    if model_name is None or model_name == '':
+        return []
+    df = pd.read_csv(f'../evaluation/csv_files/{model_name}.csv')
+    # get all unique patients as a list
+    patients = df['patient'].unique().tolist()
+    # create a list of dictionaries with the patient name and the patient name
+    options = [{'label': patient, 'value': patient} for patient in patients]
+    return options
 
 @app.callback(
     Output("bar-chart-1", "figure"),
     Input("dropdown", "value"),
+    Input("dropdown_model", "value"),
 )
-def render_bar_chart(patient):
+def render_bar_chart(patient, model_name):
+    if patient is None or patient == '':
+        return dash.no_update
+    df = pd.read_csv(f'../evaluation/csv_files/{model_name}.csv')
+
+    # get all unique patients as a list
+    patients = df['patient'].unique().tolist()
     # filter the dataframe to get only the rows for the chosen patient
     patient_df = df[df['patient'] == patient]
 
-    fig = px.bar(patient_df, x='serial_number', y='iou', orientation='v', title=f'IoU for patient : {patient}',
-                 color='iou')
+    fig = px.bar(patient_df,
+                 x='serial_number',
+                 y=["iou", "dice"],
+                 barmode='group',
+                 title=f'IoU for patient : {patient}',)
+                 #color='iou')
     # update the layout of the figure. y axis is from 0 to 1
     fig.update_layout(yaxis_range=[0, 1])
 
@@ -91,19 +118,21 @@ def render_bar_chart(patient):
 
 @app.callback(
     Output("carousel", "items"),
+    Output("carousel", "active_index"),
     Input("dropdown", "value"),
+    Input("dropdown_model", "value"),
 )
-def render_carousel(patient):
+def render_carousel(patient, model_name):
     if patient is None or patient == '':
-        return []
-    path = os.path.join('assets', patient)
+        return [], 0
+    path = os.path.join('assets', 'models_evaluation', model_name, patient)
     images = os.listdir(path)
     images = [os.path.join(path, image) for image in natsorted(images)]
     print(images)
     items = [{'src': image, 'height': 20, 'width': 100} for image in images]
-    return items
+    return items, 0
 
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8135)
+    app.run_server(debug=True, host='0.0.0.0', port=8137)
