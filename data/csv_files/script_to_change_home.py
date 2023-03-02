066ca7468adcac7a3b25224bf32a540b96ef8cfa
calde <juanfelipe.calderon@mail.polimi.it>
2023-03-02 14:57:31 +0100

    update script to use args

--- data/csv_files/script_to_change_home.py
+++ data/csv_files/script_to_change_home.py
@@ -10,7 +10,9 @@ Change the path for the HOME if we use csv files produced on another machine.
 def change_home_path(path, new_home_path):
     # split the path by os.sep
     path = path.split(os.sep)
-    postfix = os.path.join(*path[5:])
+    print(path)
+    postfix = os.path.join(*path[-6:])
+    print(new_home_path)
     new_path = os.path.join(new_home_path, postfix)
 
     return new_path
@@ -21,21 +23,23 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('home', type=str, default=constants.HOME, help='The new home path')
 
-
-
     df_train = pd.read_csv('data/csv_files/train.csv')
     df_test = pd.read_csv('data/csv_files/test.csv')
     df_val = pd.read_csv('data/csv_files/val.csv')
 
+    # get value of home
+    args = parser.parse_args()
+    home = args.home
+
     # change_home_path to X_path and y_path
-    df_train['X_path'] = df_train['X_path'].apply(lambda x: change_home_path(x, constants.HOME))
-    df_train['y_path'] = df_train['y_path'].apply(lambda x: change_home_path(x, constants.HOME))
+    df_train['X_path'] = df_train['X_path'].apply(lambda x: change_home_path(x, home))
+    df_train['y_path'] = df_train['y_path'].apply(lambda x: change_home_path(x, home))
 
-    df_test['X_path'] = df_test['X_path'].apply(lambda x: change_home_path(x, constants.HOME))
-    df_test['y_path'] = df_test['y_path'].apply(lambda x: change_home_path(x, constants.HOME))
+    df_test['X_path'] = df_test['X_path'].apply(lambda x: change_home_path(x, home))
+    df_test['y_path'] = df_test['y_path'].apply(lambda x: change_home_path(x, home))
 
-    df_val['X_path'] = df_val['X_path'].apply(lambda x: change_home_path(x, constants.HOME))
-    df_val['y_path'] = df_val['y_path'].apply(lambda x: change_home_path(x, constants.HOME))
+    df_val['X_path'] = df_val['X_path'].apply(lambda x: change_home_path(x, home))
+    df_val['y_path'] = df_val['y_path'].apply(lambda x: change_home_path(x, home))
 
     #writing the new csv files
     df_train.to_csv('data/csv_files/train.csv', index=False)
