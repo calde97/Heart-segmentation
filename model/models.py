9b16a474ba68078de44e0eb2d54b1068e6f5f782
calde <juanfelipe.calderon@mail.polimi.it>
2023-03-09 11:37:21 +0100

    add unet-lstm

--- model/models.py
+++ model/models.py
@@ -86,6 +86,100 @@ class UNet(nn.Module):
         return output_out
 
 
+
+class UNetLSTM(nn.Module):
+
+    def __init__(self, num_classes, num_sequence):
+        super(UNetLSTM, self).__init__()
+        self.num_sequence = num_sequence
+        self.num_classes = num_classes
+        self.contracting_11 = self.conv_block(in_channels=1, out_channels=64)
+        self.contracting_12 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.contracting_21 = self.conv_block(in_channels=64, out_channels=128)
+        self.contracting_22 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.contracting_31 = self.conv_block(in_channels=128, out_channels=256)
+        self.contracting_32 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.contracting_41 = self.conv_block(in_channels=256, out_channels=512)
+        self.contracting_42 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.middle = self.conv_block(in_channels=512, out_channels=1024)
+        self.expansive_11 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=3, stride=2, padding=1,
+                                               output_padding=1)
+        self.expansive_12 = self.conv_block(in_channels=1024, out_channels=512)
+        self.expansive_21 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=3, stride=2, padding=1,
+                                               output_padding=1)
+        self.expansive_22 = self.conv_block(in_channels=512, out_channels=256)
+        self.expansive_31 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=2, padding=1,
+                                               output_padding=1)
+        self.expansive_32 = self.conv_block(in_channels=256, out_channels=128)
+        self.expansive_41 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1,
+                                               output_padding=1)
+        self.expansive_42 = self.conv_block(in_channels=128, out_channels=64)
+        self.output = nn.Conv2d(in_channels=64, out_channels=num_classes, kernel_size=3, stride=1, padding=1)
+
+        self.conv_lstm_middle = ConvLSTM(512, 512, (3, 3), 3, True, True, return_all_layers=True)
+
+    def conv_block(self, in_channels, out_channels):
+        block = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=out_channels))
+        return block
+
+    # X is a tensor of shape (batch_size, num_sequence, 1, 256, 256)
+    def forward(self, X):
+        data_list = [X[0, i, ...].unsqueeze(0) for i in range(self.num_sequence)]# [-1, 64, 256, 256]
+        contracting_11_out = [self.contracting_11(data) for data in data_list]# [-1, 64, 256, 256]
+        contracting_12_out = [self.contracting_12(data) for data in contracting_11_out]# [-1, 64, 128, 128]
+        contracting_21_out = [self.contracting_21(data) for data in contracting_12_out]# [-1, 128, 128, 128]
+        contracting_22_out = [self.contracting_22(data) for data in contracting_21_out]# [-1, 128, 64, 64]
+        contracting_31_out = [self.contracting_31(data) for data in contracting_22_out]# [-1, 256, 64, 64]
+        contracting_32_out = [self.contracting_32(data) for data in contracting_31_out]# [-1, 256, 32, 32]
+        contracting_41_out = [self.contracting_41(data) for data in contracting_32_out]# [-1, 512, 32, 32]
+        contracting_42_out = [self.contracting_42(data) for data in contracting_41_out]# [-1, 512, 16, 16]
+
+        # LSTM ###############################################################################
+        # create tensor from contracting_42_out list. Each element is a sequence element
+        tensor_contracting_42_out = torch.cat(contracting_42_out, dim=0)
+        tensor_contracting_42_out = tensor_contracting_42_out.unsqueeze(0)
+        _, last_states = self.conv_lstm_middle(tensor_contracting_42_out)
+        output_lstm = [state[0] for state in last_states]
+        # LSTM ###############################################################################
+
+        middle_out = [self.middle(data) for data in output_lstm] # [-1, 1024, 16, 16]
+        expansive_11_out = [self.expansive_11(data) for data in middle_out] # [-1, 512, 32, 32]
+
+        concatenations = [torch.cat((single_expansive_11_out, single_contracting_41_out), dim=1)
+                          for single_expansive_11_out, single_contracting_41_out in zip(expansive_11_out, contracting_41_out)]
+
+        expansive_12_out = [self.expansive_12(data) for data in concatenations]  # [-1, 1024, 32, 32] -> [-1, 512, 32, 32]
+
+
+
+        expansive_21_out = [self.expansive_21(data) for data in expansive_12_out]  # [-1, 256, 64, 64]
+        concatenated_data = [torch.cat((data_expansive_21_out, data_contracting_31_out), dim=1)
+                             for data_expansive_21_out, data_contracting_31_out in zip(expansive_21_out, contracting_31_out)]
+        expansive_22_out = [self.expansive_22(data) for data in concatenated_data]  # [-1, 512, 64, 64] -> [-1, 256, 64, 64]
+        expansive_31_out = [self.expansive_31(data) for data in expansive_22_out]  # [-1, 128, 128, 128]
+
+        concatenated_data = [torch.cat((data_expansive_31_out, data_contracting_21_out), dim=1)
+                             for data_expansive_31_out, data_contracting_21_out in zip(expansive_31_out, contracting_21_out)]
+        expansive_32_out = [self.expansive_32(data) for data in concatenated_data] # [-1, 256, 128, 128] -> [-1, 128, 128, 128]
+        expansive_41_out = [self.expansive_41(data) for data in expansive_32_out]  # [-1, 64, 256, 256]
+
+        concatenated_data = [torch.cat((data_expansive_41_out, data_contracting_11_out), dim=1)
+                             for data_expansive_41_out, data_contracting_11_out in zip(expansive_41_out, contracting_11_out)]
+        expansive_42_out = [self.expansive_42(data) for data in concatenated_data]  # [-1, 128, 256, 256] -> [-1, 64, 256, 256]
+        output_out = [self.output(data) for data in expansive_42_out]
+        # transform output_out to tensor
+        output_out = torch.cat(output_out, dim=0)
+        output_out = output_out.unsqueeze(0)
+        # [-1, num_classes, 256, 256]
+        return output_out
+
+
 class Autoencoder(nn.Module):
     def __init__(self):
         super(Autoencoder, self).__init__()
@@ -524,6 +618,195 @@ class UNetGeneric(nn.Module):
         return f'{d}'
 
 
+
+import torch.nn as nn
+import torch
+
+
+class ConvLSTMCell(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, kernel_size, bias):
+        """
+        Initialize ConvLSTM cell.
+        Parameters
+        ----------
+        input_dim: int
+            Number of channels of input tensor.
+        hidden_dim: int
+            Number of channels of hidden state.
+        kernel_size: (int, int)
+            Size of the convolutional kernel.
+        bias: bool
+            Whether or not to add the bias.
+        """
+
+        super(ConvLSTMCell, self).__init__()
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+
+        self.kernel_size = kernel_size
+        self.padding = kernel_size[0] // 2, kernel_size[1] // 2
+        self.bias = bias
+
+        self.conv = nn.Conv2d(in_channels=self.input_dim + self.hidden_dim,
+                              out_channels=4 * self.hidden_dim,
+                              kernel_size=self.kernel_size,
+                              padding=self.padding,
+                              bias=self.bias)
+
+    def forward(self, input_tensor, cur_state):
+        h_cur, c_cur = cur_state
+
+        combined = torch.cat([input_tensor, h_cur], dim=1)  # concatenate along channel axis
+
+        combined_conv = self.conv(combined)
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
+        i = torch.sigmoid(cc_i)
+        f = torch.sigmoid(cc_f)
+        o = torch.sigmoid(cc_o)
+        g = torch.tanh(cc_g)
+
+        c_next = f * c_cur + i * g
+        h_next = o * torch.tanh(c_next)
+
+        return h_next, c_next
+
+    def init_hidden(self, batch_size, image_size):
+        height, width = image_size
+        return (torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device),
+                torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device))
+
+
+class ConvLSTM(nn.Module):
+
+    """
+    Parameters:
+        input_dim: Number of channels in input
+        hidden_dim: Number of hidden channels
+        kernel_size: Size of kernel in convolutions
+        num_layers: Number of LSTM layers stacked on each other
+        batch_first: Whether or not dimension 0 is the batch or not
+        bias: Bias or no bias in Convolution
+        return_all_layers: Return the list of computations for all layers
+        Note: Will do same padding.
+    Input:
+        A tensor of size B, T, C, H, W or T, B, C, H, W
+    Output:
+        A tuple of two lists of length num_layers (or length 1 if return_all_layers is False).
+            0 - layer_output_list is the list of lists of length T of each output
+            1 - last_state_list is the list of last states
+                    each element of the list is a tuple (h, c) for hidden state and memory
+    Example:
+        >> x = torch.rand((32, 10, 64, 128, 128))
+        >> convlstm = ConvLSTM(64, 16, 3, 1, True, True, False)
+        >> _, last_states = convlstm(x)
+        >> h = last_states[0][0]  # 0 for layer index, 0 for h index
+    """
+
+    def __init__(self, input_dim, hidden_dim, kernel_size, num_layers,
+                 batch_first=False, bias=True, return_all_layers=False):
+        super(ConvLSTM, self).__init__()
+
+        self._check_kernel_size_consistency(kernel_size)
+
+        # Make sure that both `kernel_size` and `hidden_dim` are lists having len == num_layers
+        kernel_size = self._extend_for_multilayer(kernel_size, num_layers)
+        hidden_dim = self._extend_for_multilayer(hidden_dim, num_layers)
+        if not len(kernel_size) == len(hidden_dim) == num_layers:
+            raise ValueError('Inconsistent list length.')
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.kernel_size = kernel_size
+        self.num_layers = num_layers
+        self.batch_first = batch_first
+        self.bias = bias
+        self.return_all_layers = return_all_layers
+
+        cell_list = []
+        for i in range(0, self.num_layers):
+            cur_input_dim = self.input_dim if i == 0 else self.hidden_dim[i - 1]
+
+            cell_list.append(ConvLSTMCell(input_dim=cur_input_dim,
+                                          hidden_dim=self.hidden_dim[i],
+                                          kernel_size=self.kernel_size[i],
+                                          bias=self.bias))
+
+        self.cell_list = nn.ModuleList(cell_list)
+
+    def forward(self, input_tensor, hidden_state=None):
+        """
+        Parameters
+        ----------
+        input_tensor: todo
+            5-D Tensor either of shape (t, b, c, h, w) or (b, t, c, h, w)
+        hidden_state: todo
+            None. todo implement stateful
+        Returns
+        -------
+        last_state_list, layer_output
+        """
+        if not self.batch_first:
+            # (t, b, c, h, w) -> (b, t, c, h, w)
+            input_tensor = input_tensor.permute(1, 0, 2, 3, 4)
+
+        b, _, _, h, w = input_tensor.size()
+
+        # Implement stateful ConvLSTM
+        if hidden_state is not None:
+            raise NotImplementedError()
+        else:
+            # Since the init is done in forward. Can send image size here
+            hidden_state = self._init_hidden(batch_size=b,
+                                             image_size=(h, w))
+
+        layer_output_list = []
+        last_state_list = []
+
+        seq_len = input_tensor.size(1)
+        cur_layer_input = input_tensor
+
+        for layer_idx in range(self.num_layers):
+
+            h, c = hidden_state[layer_idx]
+            output_inner = []
+            for t in range(seq_len):
+                h, c = self.cell_list[layer_idx](input_tensor=cur_layer_input[:, t, :, :, :],
+                                                 cur_state=[h, c])
+                output_inner.append(h)
+
+            layer_output = torch.stack(output_inner, dim=1)
+            cur_layer_input = layer_output
+
+            layer_output_list.append(layer_output)
+            last_state_list.append([h, c])
+
+        if not self.return_all_layers:
+            layer_output_list = layer_output_list[-1:]
+            last_state_list = last_state_list[-1:]
+
+        return layer_output_list, last_state_list
+
+    def _init_hidden(self, batch_size, image_size):
+        init_states = []
+        for i in range(self.num_layers):
+            init_states.append(self.cell_list[i].init_hidden(batch_size, image_size))
+        return init_states
+
+    @staticmethod
+    def _check_kernel_size_consistency(kernel_size):
+        if not (isinstance(kernel_size, tuple) or
+                (isinstance(kernel_size, list) and all([isinstance(elem, tuple) for elem in kernel_size]))):
+            raise ValueError('`kernel_size` must be tuple or list of tuples')
+
+    @staticmethod
+    def _extend_for_multilayer(param, num_layers):
+        if not isinstance(param, list):
+            param = [param] * num_layers
+        return param
+
+
 def get_model_from_name(name, in_channels=1):
     if name == constants.UNET_MODEL:
         return UNet(num_classes=1)
@@ -539,6 +822,8 @@ def get_model_from_name(name, in_channels=1):
                            normalization='batch',
                            conv_mode='same',
                            dim=2)
+
+    # TODO add UnetLSTM
 
     else:
         raise ValueError('Unknown model name: {}'.format(name))
